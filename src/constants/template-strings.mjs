278c60bb000648a7b0e30aae22a39db099c61ee6
ojas kapre <okapre@enlil.com>
2022-12-15 18:04:03 +0530

    style: updated css for Copied icon refactor: updated class name for result card css based on standard naming convention

--- src/constants/template-strings.mjs
+++ src/constants/template-strings.mjs
@@ -1,22 +1,22 @@
 export function getRefreshIconSvg(width = '1em', height = '1em', color = 'red') {
   return `
-<svg id="refreshIconSvg" width=${width} height=${height} viewBox="0 0 16 16" fill="${color}" class="bi bi-arrow-clockwise" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M3.17 6.706a5 5 0 0 1 7.103-3.16.5.5 0 1 0 .454-.892A6 6 0 1 0 13.455 5.5a.5.5 0 0 0-.91.417 5 5 0 1 1-9.375.789z"/>
-  <path fill-rule="evenodd" d="M8.147.146a.5.5 0 0 1 .707 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 1 1-.707-.708L10.293 3 8.147.854a.5.5 0 0 1 0-.708z"/>
-</svg>
+  <svg id="refresh-icon-svg" width=${width} height=${height} viewBox="0 0 16 16" fill="${color}" class="bi bi-arrow-clockwise" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M3.17 6.706a5 5 0 0 1 7.103-3.16.5.5 0 1 0 .454-.892A6 6 0 1 0 13.455 5.5a.5.5 0 0 0-.91.417 5 5 0 1 1-9.375.789z"/>
+    <path fill-rule="evenodd" d="M8.147.146a.5.5 0 0 1 .707 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 1 1-.707-.708L10.293 3 8.147.854a.5.5 0 0 1 0-.708z"/>
+  </svg>
 `
 }
 
 export function getCopyIconSvg(width = '1em', height = '1em', color = 'red') {
   return `
-  <svg id="copyIconSvg" width=${width} height=${height} fill="${color}" viewBox="0 0 25 25">
+  <svg id="copy-icon-svg" width=${width} height=${height} fill="${color}" viewBox="0 0 25 25">
     <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
   </svg>
   `
 }
 export function getNewTabIconSvg(width = '1em', height = '1em', color = 'red') {
   return `
-  <svg id="newTabSvg" width=${width} height=${height} fill="${color}" viewBox="0 0 44 44"  xmlns="http://www.w3.org/2000/svg">
+  <svg id="new-tab-svg" width=${width} height=${height} fill="${color}" viewBox="0 0 44 44"  xmlns="http://www.w3.org/2000/svg">
     <path d="M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h13.95v3H9v30h30V25.05h3V39q0 1.2-.9 2.1-.9.9-2.1.9Zm10.1-10.95L17 28.9 36.9 9H25.95V6H42v16.05h-3v-10.9Z"/>
   </svg>
   `
@@ -40,27 +40,27 @@ export function getViewHistoryIcon(width = '1em', height = '1em', color = 'red')
 
 export function getResultCardTemplate(iconColor) {
   return `
-  <div id="gptxCardHeader">
-  <span id="gptxLoadingPara">Loading results from ChatGPT...</span>
-  <span id="gptxTimePara"></span>
-</div>
-<div id="gptxCardBody">
-  <div id="gptxResponseBody" class="markdown-body" dir="auto"></div>
-</div>
-<div id="gptxCardFooter">
-  <div class="btn gptxFooterBtns" id="gptxFooterNewTabBtn">
-    ${getNewTabIconSvg('1.2em', '1.2em', iconColor)}
-    <span class="gptx-tooltip-text">Open in new tab</span>
+  <div id="gptx-card-header">
+    <span id="gptx-loading-para">Loading results from ChatGPT...</span>
+    <span id="gptx-time-para"></span>
   </div>
-  <div class="btn gptxFooterBtns" id="gptxFooterCopyBtn">
-    ${getCopyIconSvg('1.2em', '1.2em', iconColor)}
-    <span class="gptx-tooltip-text">Copy</span>
+  <div id="gptx-card-body">
+    <div id="gptx-response-body" class="markdown-body" dir="auto"></div>
   </div>
-  <div class="btn gptxFooterBtns" id="gptxFooterRefreshBtn">
-    ${getRefreshIconSvg('1.2em', '1.2em', iconColor)}
-    <span class="gptx-tooltip-text">Refresh</span>
+  <div id="gptx-card-footer">
+    <div class="btn gptx-footer-btns" id="gptx-footer-new-tab-btn">
+      ${getNewTabIconSvg('1.2em', '1.2em', iconColor)}
+      <span class="gptx-tooltip-text">Open in new tab</span>
+    </div>
+    <div class="btn gptx-footer-btns" id="gptx-footer-copy-btn">
+      ${getCopyIconSvg('1.2em', '1.2em', iconColor)}
+      <span class="gptx-tooltip-text" id="gptx-tooltip-copy-text">Copy</span>
+    </div>
+    <div class="btn gptx-footer-btns" id="gptx-footer-refresh-btn">
+      ${getRefreshIconSvg('1.2em', '1.2em', iconColor)}
+      <span class="gptx-tooltip-text">Refresh</span>
+    </div>
   </div>
-</div>
   `
 }
 
--- src/content-script/index.mjs
+++ src/content-script/index.mjs
@@ -34,7 +34,7 @@ async function run(question) {
       .getComputedStyle(document.getElementById('center_col'), null)
       .getPropertyValue('margin-left')
     const newNode = document.createElement('div') // created element to insert chatgpt results content
-    newNode.classList.add('gptxCard')
+    newNode.classList.add('gptx-card')
 
     // set colors to footer icons based on dark/light mode
     let footerBtnsIconSvgColor
@@ -61,12 +61,12 @@ async function run(question) {
 
     const port = Browser.runtime.connect() // defined port to send questions to background worker
 
-    const gptxCardHeaderElem = document.getElementById('gptxCardHeader')
-    const gptxLoadingParaElem = document.getElementById('gptxLoadingPara')
-    const gptxResponseBodyElem = document.getElementById('gptxResponseBody')
-    const gptxFooterRefreshBtn = document.getElementById('gptxFooterRefreshBtn')
-    const gptxFooterCopyBtn = document.getElementById('gptxFooterCopyBtn')
-    const gptxFooterNewTabBtn = document.getElementById('gptxFooterNewTabBtn')
+    const gptxCardHeaderElem = document.getElementById('gptx-card-header')
+    const gptxLoadingParaElem = document.getElementById('gptx-loading-para')
+    const gptxResponseBodyElem = document.getElementById('gptx-response-body')
+    const gptxFooterRefreshBtn = document.getElementById('gptx-footer-refresh-btn')
+    const gptxFooterCopyBtn = document.getElementById('gptx-footer-copy-btn')
+    const gptxFooterNewTabBtn = document.getElementById('gptx-footer-new-tab-btn')
 
     let startTime = performance.now()
     let previousResponse
@@ -76,9 +76,9 @@ async function run(question) {
       if (msg.answer) {
         // add {question: answer} to local storage at end of answer
         if (msg.answer === 'CHAT_GPTX_ANSWER_END') {
-          gptxFooterRefreshBtn.classList.remove('gptxDisableBtn')
-          gptxFooterCopyBtn.classList.remove('gptxDisableBtn')
-          gptxFooterNewTabBtn.classList.remove('gptxDisableBtn')
+          gptxFooterRefreshBtn.classList.remove('gptx-disable-btn')
+          gptxFooterCopyBtn.classList.remove('gptx-disable-btn')
+          gptxFooterNewTabBtn.classList.remove('gptx-disable-btn')
           Browser.storage.local
             .set({
               [question]: previousResponse,
@@ -88,18 +88,18 @@ async function run(question) {
             })
         } else {
           previousResponse = msg.answer
-          gptxFooterCopyBtn.classList.add('gptxDisableBtn')
-          gptxFooterNewTabBtn.classList.add('gptxDisableBtn')
+          gptxFooterCopyBtn.classList.add('gptx-disable-btn')
+          gptxFooterNewTabBtn.classList.add('gptx-disable-btn')
           updateResultDOM(msg.answer, startTime)
         }
       } else if (msg.error === 'UNAUTHORIZED') {
-        gptxFooterRefreshBtn.classList.remove('gptxDisableBtn')
+        gptxFooterRefreshBtn.classList.remove('gptx-disable-btn')
         gptxLoadingParaElem.style.display = 'none'
         gptxResponseBodyElem.style['margin-top'] = '0px'
         gptxResponseBodyElem.innerHTML =
           '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>'
       } else {
-        gptxFooterRefreshBtn.classList.remove('gptxDisableBtn')
+        gptxFooterRefreshBtn.classList.remove('gptx-disable-btn')
         gptxLoadingParaElem.style.display = 'none'
         gptxResponseBodyElem.style['margin-top'] = '0px'
         gptxResponseBodyElem.innerHTML = '<p>Failed to load response from ChatGPT</p>'
@@ -118,9 +118,9 @@ async function run(question) {
       console.log('GPTX: cached result used')
       updateResultDOM(cachedQuestion[question], startTime)
     } else {
-      gptxFooterRefreshBtn.classList.add('gptxDisableBtn')
-      gptxFooterCopyBtn.classList.add('gptxDisableBtn')
-      gptxFooterNewTabBtn.classList.add('gptxDisableBtn')
+      gptxFooterRefreshBtn.classList.add('gptx-disable-btn')
+      gptxFooterCopyBtn.classList.add('gptx-disable-btn')
+      gptxFooterNewTabBtn.classList.add('gptx-disable-btn')
       port.postMessage({ question })
     }
 
@@ -128,16 +128,22 @@ async function run(question) {
     gptxFooterRefreshBtn.addEventListener('click', () => {
       console.log('gptx refresh result')
       startTime = performance.now()
-      gptxFooterRefreshBtn.classList.add('gptxDisableBtn')
-      gptxFooterCopyBtn.classList.add('gptxDisableBtn')
-      gptxFooterNewTabBtn.classList.add('gptxDisableBtn')
+      gptxFooterRefreshBtn.classList.add('gptx-disable-btn')
+      gptxFooterCopyBtn.classList.add('gptx-disable-btn')
+      gptxFooterNewTabBtn.classList.add('gptx-disable-btn')
       port.postMessage({ question })
     })
     gptxFooterCopyBtn.addEventListener('click', async () => {
       cachedQuestion = await Browser.storage.local.get(question)
-      gptxFooterCopyBtn.innerHTML = getApprovedCheckIconSvg('1.2em', '1.2em', 'green')
+      gptxFooterCopyBtn.innerHTML = `
+      ${getApprovedCheckIconSvg('1.2em', '1.2em', '#198754')}
+      <span class="gptx-tooltip-text" id="gptx-tooltip-copied-text">Copied!!!</span>
+      `
       setTimeout(() => {
-        gptxFooterCopyBtn.innerHTML = getCopyIconSvg('1.2em', '1.2em', 'white')
+        gptxFooterCopyBtn.innerHTML = `
+        ${getCopyIconSvg('1.2em', '1.2em', 'white')}
+        <span class="gptx-tooltip-text" id="gptx-tooltip-copy-text">Copy</span>
+        `
       }, 700)
       clipboard.write(cachedQuestion[question]).then(() => {
         console.log('gptx result copied')
@@ -149,10 +155,10 @@ async function run(question) {
   }
   function updateResultDOM(text, startTime) {
     // Updates DOM if result is returned or is already cached
-    const gptxCardHeaderElem = document.getElementById('gptxCardHeader')
-    const gptxLoadingParaElem = document.getElementById('gptxLoadingPara')
-    const gptxTimeParaElem = document.getElementById('gptxTimePara')
-    const gptxResponseBodyElem = document.getElementById('gptxResponseBody')
+    const gptxCardHeaderElem = document.getElementById('gptx-card-header')
+    const gptxLoadingParaElem = document.getElementById('gptx-loading-para')
+    const gptxTimeParaElem = document.getElementById('gptx-time-para')
+    const gptxResponseBodyElem = document.getElementById('gptx-response-body')
     const markdown = new MarkdownIt()
     let endTime
     gptxLoadingParaElem.innerHTML = 'ChatGPT powered results'
